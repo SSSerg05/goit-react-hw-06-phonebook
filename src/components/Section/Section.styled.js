fb4ea81e4f567cd0db36c1cb6b8c87b868f55b76
SSSerg05 <ssserg05@gmail.com>
2023-10-03 11:27:02 +0300

    fix

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -3,7 +3,8 @@ import styled from '@emotion/styled';
 export const Title = styled.h3`
   margin: 0;
   padding: 0;
-  margin-top: 30px;
+  margin-top: 32px;
+  margin-bottom: 16px;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 24px;
   font-weight: 700;
