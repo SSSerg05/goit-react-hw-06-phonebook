7b075bfb0b9b69d5f3899ead146ae3c902c5c195
SSSerg05 <ssserg05@gmail.com>
2023-10-04 18:02:01 +0300

    fix style

--- src/components/Phonebook/Form/Form.styled.jsx
+++ src/components/Phonebook/Form/Form.styled.jsx
@@ -44,18 +44,21 @@ export const Button = styled.button`
   outline: 1px solid black;
   border-radius: 10px;
   transition: background-color 150ms ease-in, 
+              color 150ms ease-in,
               font-weight 150ms ease-in;
 
   &:hover, &:active {
     cursor: pointer;
     font-weight: 700;
     letter-spacing: 0.03rem;  
-    background-color: cyan;
+    background-color: #1976d2;
+    color: #fff;
     border: 1px solit black;
   }
 
   &:active {
     background-color: tomato;
-    transition: background-color 150ms linear;
+    transition: background-color 150ms ease-in,
+                color 150ms ease-in;
   }
 `
